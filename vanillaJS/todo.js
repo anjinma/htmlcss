4e069f973b180388709d0c20e6701d19aef71b37
anjinma <hellocworld@naver.con>
2018-12-28 20:23:30 +0900

    todo  delete 완료

--- vanillaJS/todo.js
+++ vanillaJS/todo.js
@@ -3,18 +3,29 @@ const todInput = todForm.querySelector("input");
 const todList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
 
-function saveToDo(){
-    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    todList.removeChild(li);
+    const cleanToDos = toDos.filter(function(todo){
+        return todo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDo();
 }
 
+function saveToDo(event){
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length+1;
     delBtn.innerText = "X";
+    delBtn.addEventListener("click",deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
